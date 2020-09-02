d7b9965c9dc9e019c2760e734129e735aa75c786
alexmalinowski <70277551+alexmalinowski@users.noreply.github.com>
2020-09-02 17:03:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.body.querySelector("#helloDiv").innerHTML="Hello World!";
+var prompter = prompt("Please enter your name in the box below.");
+document.body.querySelector("#prompterDiv").innerHTML=prompter;
